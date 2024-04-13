072717a3cb0f29015aa21c09cb1bf3a256025128
Jordan <119638801+PlebSauce@users.noreply.github.com>
2024-04-13 01:26:24 -0700

    Boilerplate golang backend stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/PlebSauce/PersonalChessSite
 
 go 1.22.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+	github.com/notnil/chess v1.9.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	test_drive.exampleGame()
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in environment")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Printf("Starting server on port %v", portString)
+	srv.ListenAndServe()
 }
